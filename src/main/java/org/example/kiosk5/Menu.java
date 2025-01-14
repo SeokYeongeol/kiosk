8cbde9829fa63542169a28d2e8ef4a104fdb39eb
cheoni5417 <cheoni5417@naver.com>
2025-01-14 21:36:54 +0900

    refactor : 메뉴 리스트의 index 위치의 값을 조회하는 메서드를 추가해 조회하는 명령들을 리팩토링

--- src/main/java/org/example/kiosk5/Menu.java
+++ src/main/java/org/example/kiosk5/Menu.java
@@ -12,13 +12,15 @@ public class Menu {
     // setter
     public void setMenuItemList(MenuItem menuItem) { this.menuItemList.add(menuItem); }
 
+    public MenuItem getMenuItem(int index) { return getMenuItemList().get(index); }
+
     public void lookUpMenuList() {      // 메뉴 리스트 조회
         for(int i = 0; i < getMenuItemList().size(); i++) {
-            System.out.println((i+1) + ". " + getMenuItemList().get(i).getName() + "   | W " + getMenuItemList().get(i).getPrice() + " | " + getMenuItemList().get(i).getDesc());
+            System.out.println((i+1) + ". " + getMenuItem(i).getName() + "   | W " + getMenuItem(i).getPrice() + " | " + getMenuItem(i).getDesc());
         }
     }
 
     public void lookUpMenu(String value) {      // 선택한 메뉴 조회
-        System.out.println("선택한 메뉴 : " + getMenuItemList().get(Integer.parseInt(value)-1).getName() + "   | W " + getMenuItemList().get(Integer.parseInt(value)-1).getPrice() + " | " + getMenuItemList().get(Integer.parseInt(value)-1).getDesc());
+        System.out.println("선택한 메뉴 : " + getMenuItem(Integer.parseInt(value)-1).getName() + "   | W " + getMenuItem(Integer.parseInt(value)-1).getPrice() + " | " + getMenuItem(Integer.parseInt(value)-1).getDesc());
     }
 }
